4acc841611afeb9adb9d4a4861989099284cc817
jlee8020 <59134780+jlee8020@users.noreply.github.com>
2020-05-21 22:46:03 -0500

    refactor search code with proper state

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 
 
 ### Credits 
--Images [Robohash]("https://robohash.org/1?set=set2")
+[Robohash](https://robohash.org)
 
 
 ### Deployment
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {CardList} from './components/card-list/card-list.component'
-
+import {SearchBox} from './components/search-box/search-box.component'
 import './App.css';
 
 class App extends Component {
@@ -8,8 +8,10 @@ class App extends Component {
     super();
     
     this.state = {
-    monsters : []
-  };
+    monsters : [],
+    searchField: ''
+    };
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount(){
@@ -19,10 +21,25 @@ class App extends Component {
     .then(users => this.setState({monsters: users}));
 
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value})
+
+  }
   render() {
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase()
+      .includes(searchField.toLowerCase())
+    )
+      
     return (
           <div className="App">
-          <CardList monsters={this.state.monsters}/>
+          <SearchBox
+            placeholder='search monsters'
+            handleChange={this.handleChange}
+          />
+          <CardList monsters={filteredMonsters}/>
           </div>
     );
   }
